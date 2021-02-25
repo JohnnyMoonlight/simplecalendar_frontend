f250d2b19c97a27fc835ce2e5dae593f650d6d54
Tobias Hellmund <tobias.hellmund@iosb.fraunhofer.de>
2021-02-25 20:27:42 +0100

    show different views for calendar

---
+++ src/components/Loader.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+
+  <section class="">
+    <div v-if="this.loading == true" class="backdrop">
+      <span class="loader" >Loading...<font-awesome-icon :icon="['fas', 'spinner']" spin /></span>
+    </div>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Loader',
+     mounted () {
+
+    },
+    props: ['loading'],
+    data () {
+      return {
+        
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style>
+  .backdrop {
+    background: rgba(87, 87, 83, 0.5);
+    position:fixed;
+    height: 110%;
+    width: 110%;
+    z-index: 10000;
+    top:-10%;
+    left:-10%
+  }
+  .loader{
+    position: relative;
+    background-color: rgba(2, 116, 33, 0.733);
+    border-radius: 5px;
+    padding: 15px;
+    top:40%;
+    left:3%;
+  }
+</style>
